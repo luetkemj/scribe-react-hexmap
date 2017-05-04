efd93884eced4b1cf78a26cd763149f0d46af168
Mark Luetke <luetkemj@gmail.com>
2017-05-03 22:28:08 -0500

    demo

--- src/index.js
+++
@@ -1,4 +0,0 @@
-import Hexmap from './components/hexmap/hexmap.component';
-
-export HexmapTestContainer from './containers/hexmap/hexmap.container';
-export default Hexmap;
--- stories/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@kadira/storybook';
-import HexmapContainer from '../src/containers/hexmap/hexmap.container';
-
-storiesOf('Hexmap', module)
-  .add('Hexmap Container', () => (
-    <HexmapContainer />
-  ));
